033342877f598a857fed5002735e694db7df4629
shubhindia <shubhindia123@gmail.com>
2024-07-22 15:15:06 +0530

    add python lsp config
    Signed-off-by: shubhindia <shubhindia123@gmail.com>

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -56,6 +56,12 @@ return {
           },
         },
       })
+      lspconfig.pyright.setup({
+        capabilities = capabilities
+      })
+      lspconfig.yamlls.setup({
+        capabilities = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
